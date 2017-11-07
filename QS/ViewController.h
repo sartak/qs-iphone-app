247942c8c2e76ec4e67eff4111bcab9c1243d4c9
Shawn M Moore <code@sartak.org>
2017-11-06 21:20:25 -0500

    Initial Commit

---
+++ QS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QS
+//
+//  Created by Shawn Moore on 2017/11/06.
+//  Copyright © 2017年 Shawn Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QS
+//
+//  Created by Shawn Moore on 2017/11/06.
+//  Copyright © 2017年 Shawn Moore. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
